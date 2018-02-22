86a3ecb8234bf4f0080a8d7f25bb60a110a51d1f
eteubert <eric@ericteubert.de>
2018-02-22 23:39:23 +0800

    interpret first line

--- src/Podlove/Webvtt/parser.php
+++ src/Podlove/Webvtt/parser.php
@@ -2,8 +2,26 @@
 namespace Podlove\Webvtt;
 
 class Parser {
-	function hello()
+	public static function parse($str)
 	{
-		return 'world';
+		$pos = 0;
+		$messages = [];
+
+		// skip optional BOM
+		$bom = chr(239) . chr(187) . chr(191);
+		if (substr($str, $pos, 3) == $bom) {
+			$pos += 3;
+		}
+
+		if (substr($str, $pos, 6) == "WEBVTT") {
+			$pos += 6;
+		} else {
+			$messages[] = "Missing WEBVTT at beginning of file.";
+		}
+
+		return [
+			'result' => [],
+			'messages' => $messages
+		];
 	}
 }
--- test/ParserTest.php
+++ test/ParserTest.php
@@ -5,8 +5,38 @@ use Podlove\Webvtt\Parser;
 
 class ParserTest extends TestCase
 {
-    public function testHello()
+    public function testEmpty()
     {
-        $this->assertEquals((new Parser)->hello(), 'world');
+        $content = <<<DOC
+WEBVTT
+
+DOC;
+        $this->assertEquals(Parser::parse($content), self::empty_result());
+    }
+
+    function testIgnoreBOM()
+    {
+        $bom = chr(239) . chr(187) . chr(191);
+        $content = <<<DOC
+WEBVTT
+
+DOC;
+        $this->assertEquals(Parser::parse($bom . $content), self::empty_result());
+    }
+
+    function testMissingWEBVTT()
+    {
+        $this->assertEquals(Parser::parse(""), [
+            'result' => [],
+            'messages' => ["Missing WEBVTT at beginning of file."]
+        ]);
+    }
+
+    private static function empty_result()
+    {
+        return [
+            'result' => [],
+            'messages' => []
+        ];
     }
 }
